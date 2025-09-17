66fc70e9c53491fb3e38e8dd69ab22acba69e7a0
Rafy01 <150427662+Rafy01@users.noreply.github.com>
2025-09-18 00:51:58 +0200

    fix: update baseUrl in siteMeta to production URL

--- src/lib/siteMeta.ts
+++ src/lib/siteMeta.ts
@@ -6,7 +6,7 @@
 // };
 export const site = {
   name: "Fyrrehaven 61",
-  baseUrl: "http://localhost:5173", // skift til https://fyrrehaven-61.dk ved deploy
+  baseUrl: "https://fyrrehaven-61.dk", // skift til https://fyrrehaven-61.dk ved deploy
   twitter: "", // fx "@fyrrehaven61" ellers tom streng
   defaultImage: "/og-default.jpg",
   robots: "index, follow",
